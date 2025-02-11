11e5d572520c5b4eee29192ddb397e5c4a3421e1
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2025-02-11 20:10:09 +0530

    Update daoip-5/scripts/API/run.py

--- daoip-5/scripts/API/run.py
+++ daoip-5/scripts/API/run.py
@@ -307,5 +307,5 @@ def search_project(project_name):
 app.register_blueprint(allo_blueprint, url_prefix='/allo')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
